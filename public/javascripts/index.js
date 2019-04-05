31a04221ab5f819426f1592348ef431aa407e709
竹内一将 <kazumasa.takeuchi@mb.lecip.co.jp>
2019-04-05 16:05:15 +0900

    add voltage and luminous

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
   var timeData = [],
     temperatureData = [],
+    powervoltageData = [],
+    luminousData = [],
     humidityData = [];
   var data = {
     labels: timeData,
@@ -15,6 +17,28 @@ $(document).ready(function () {
         pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
         pointHoverBorderColor: "rgba(255, 204, 0, 1)",
         data: temperatureData
+      },
+      {
+        fill: false,
+        label: 'Power Voltage',
+        yAxisID: 'Power Voltage',
+        borderColor: "rgba(203, 27, 69, 1)",
+        pointBoarderColor: "rgba(203, 27, 69, 1)",
+        backgroundColor: "rgba(203, 27, 69, 0.4)",
+        pointHoverBackgroundColor: "rgba(203, 27, 69, 1)",
+        pointHoverBorderColor: "rgba(203, 27, 69, 1)",
+        data: powervoltageData
+      },
+      {
+        fill: false,
+        label: 'Luminous intensity',
+        yAxisID: 'Luminous intensity',
+        borderColor: "rgba(38, 135, 133, 1)",
+        pointBoarderColor: "rgba(38, 135, 133, 1)",
+        backgroundColor: "rgba(38, 135, 133, 0.4)",
+        pointHoverBackgroundColor: "rgba(38, 135, 133, 1)",
+        pointHoverBorderColor: "rgba(38, 135, 133, 1)",
+        data: luminousData
       },
       {
         fill: false,
@@ -33,7 +57,7 @@ $(document).ready(function () {
   var basicOption = {
     title: {
       display: true,
-      text: 'Temperature & Humidity Real-time Data',
+      text: 'Highway LED Board Real-time Data 1',
       fontSize: 36
     },
     scales: {
@@ -57,6 +81,33 @@ $(document).ready(function () {
     }
   }
 
+  var basicOption2 = {
+    title: {
+      display: true,
+      text: 'Highway LED Board Real-time Data 2',
+      fontSize: 36
+    },
+    scales: {
+      yAxes: [{
+        id: 'Power Voltage',
+        type: 'linear',
+        scaleLabel: {
+          labelString: 'Voltage(V)',
+          display: true
+        },
+        position: 'left',
+      }, {
+          id: 'Luminous intensity',
+          type: 'linear',
+          scaleLabel: {
+            labelString: 'Luminous intensity(Lx)',
+            display: true
+          },
+          position: 'right'
+        }]
+    }
+  }
+
   //Get the context of the canvas element we want to select
   var ctx = document.getElementById("myChart").getContext("2d");
   var optionsNoAnimation = { animation: false }
@@ -64,6 +115,13 @@ $(document).ready(function () {
     type: 'line',
     data: data,
     options: basicOption
+  });
+
+  var ctx2 = document.getElementById("myChart2").getContext("2d");
+  var myLineChart2 = new Chart(ctx2, {
+    type: 'line',
+    data: data,
+    options: basicOption2
   });
 
   var ws = new WebSocket('ws://' + location.host);
@@ -85,6 +143,27 @@ $(document).ready(function () {
       if (len > maxLen) {
         timeData.shift();
         temperatureData.shift();
+      }
+
+      if (obj.powervoltage) {
+        powervoltageData.push(obj.powervoltage);
+      }
+      if (powervoltageData.length > maxLen) {
+        powervoltageData.shift();
+      }
+
+      if (obj.luminous) {
+        luminousData.push(obj.luminous);
+      }
+      if (luminousData.length > maxLen) {
+        luminousData.shift();
+      }
+
+      if (obj.humidity) {
+        humidityData.push(obj.humidity);
+      }
+      if (humidityData.length > maxLen) {
+        humidityData.shift();
       }
 
       if (obj.humidity) {
